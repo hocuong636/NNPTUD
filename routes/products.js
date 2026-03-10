91a7f3ae83b5773a7f2aa837fbb0b2eb1ce86ac7
hocuong636 <hocuong636@gmail.com>
2026-03-10 14:00:18 +0700

    products

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,75 @@
 var express = require('express');
 var router = express.Router();
-let productModel = require('../schemas/products')
+let productModel = require('../schemas/products');
+const { default: slugify } = require('slugify');
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
-  let result = await productModel.find({}) 
+/* GET all products */
+router.get('/', async function (req, res, next) {
+  let result = await productModel.find({})
   res.send(result);
 });
-router.get('/id', function(req, res, next) {
-  res.send('hahah');
+
+router.get('/:id', async function (req, res, next) {
+  try {
+    let id = req.params.id;
+    let result = await productModel.findOne({
+      isDeleted: false,
+      _id: id
+    })
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(404).send({ message: "ID NOT FOUND" });
+    }
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+router.post('/', async function (req, res, next) {
+  try {
+    let newProduct = new productModel({
+      title: req.body.title,
+      slug: slugify(req.body.title, {
+        replacement: '-',
+        remove: undefined,
+        lower: true,
+        strict: false,
+      }),
+      price: req.body.price,
+      description: req.body.description,
+      images: req.body.images,
+      category: req.body.category
+    });
+    await newProduct.save();
+    res.send(newProduct)
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+router.put('/:id', async function (req, res, next) {
+  try {
+    let id = req.params.id;
+    let updatedItem = await productModel.findByIdAndUpdate(id, req.body, {
+      new: true
+    });
+    res.send(updatedItem)
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+
+router.delete('/:id', async function (req, res, next) {
+  try {
+    let id = req.params.id;
+    let updatedItem = await productModel.findByIdAndUpdate(id, {
+      isDeleted: true
+    }, {
+      new: true
+    });
+    res.send(updatedItem)
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
 });
 
 module.exports = router;
